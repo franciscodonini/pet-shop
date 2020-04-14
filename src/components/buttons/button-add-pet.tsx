89109fdfc8bb710a4f5655c267ec8ed38a1db6ce
franciscodonini <franciscodoniniramos@gmail.com>
2020-04-14 20:46:15 -0300

    criado arquivo botão da agenda

--- src/components/buttons/button-add-pet.tsx
+++ src/components/buttons/button-add-pet.tsx
@@ -5,9 +5,6 @@ import './button-add-pet.css';
 
 export default class ButtonAdd extends React.Component {
     public render() {
-        return <div>
-                    <button id= "addPet">Adicionar Pet</button>             
-
-               </div>
+        return  <button id= "addPet">Adicionar Pet</button>            
     }
 }
