4c7f7d10f0b5dbd8290a84302cb865549a51c7be
franciscodonini <franciscodoniniramos@gmail.com>
2020-04-19 15:52:49 -0300

    pages add

---
+++ src/pages/agenda.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Agenda extends React.Component {
+    public render () {
+        return (
+            <h1>AGENDA</h1>
+        )
+    }
+}
---
+++ src/pages/fluxo-caixa.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class FluxoDeCaixa extends React.Component {
+    public render () {
+        return (
+            <h1>FLUXO DE CAIXA</h1>
+        )
+    }
+}
